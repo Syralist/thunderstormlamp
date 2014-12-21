0837aad5cec6a86418701e7c8a2fc07eb24c0117
Thomas Helmke <thomas.helmke@gmx.de>
2014-12-21 17:24:53 +0100

    installed first patterns into cloud.
    Signed-off-by: Thomas Helmke <thomas.helmke@gmx.de>

--- Arduino/cloud01/src/cloud01.ino
+++ Arduino/cloud01/src/cloud01.ino
@@ -27,13 +27,25 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXEL, PIN, NEO_GRB | NEO_KHZ800)
 //cycle counter
 volatile unsigned int cycles = 0;
 
+//variables for the flash functions
+volatile unsigned long flashCount = 0;
+volatile unsigned long loopCount = 0;
+volatile float period = 0;
+volatile int pixel = 1;
+volatile int pause = 0;
+
+//global parameters
+float maxbrightness = 127.0;
+const float cycle = 10;
+
 //variables for decade switch
 unsigned int last_dek_change = 0;
 unsigned int letzte_dekade = 0;
 bool neue_dekade = false;
+unsigned int dekade = 0;
 
 //variables for led adressing
-int ledmatrix[ROWS][COLUMNS] = {{61, 60, 59, 58, 57,  1,  2,  3,  4,  5,  6,  7},
+const int ledmatrix[ROWS][COLUMNS] = {{61, 60, 59, 58, 57,  1,  2,  3,  4,  5,  6,  7},
                                 {62, 63, 64, 65, 66, 14, 13, 12, 11, 10,  9,  8},
                                 {71, 70, 69, 68, 67, 15, 16, 17, 18, 19, 20, 21},
                                 {72, 73, 74, 75, 76, 28, 27, 26, 25, 24, 23, 22},
@@ -59,13 +71,92 @@ void setup() {
 /*****Flash*****/
 void flash()
 {
+    float brightness = 0.0;
+    uint32_t color = 0;
+    uint32_t colorfade = 0;
+
     cycles++;
+    switch(dekade)
+    {
+        case 1:
+            period = 4000;
+            brightness = pattern2(flashCount*cycle/period);
+            flashCount++;
+            color = strip.Color(maxbrightness,maxbrightness,maxbrightness);
+            colorfade = strip.Color(brightness*maxbrightness,brightness*maxbrightness,brightness*maxbrightness);
+            setAllPixels(color);
+            strip.setPixelColor(pixel, colorfade);
+            if(flashCount * cycle == period)
+            {
+                flashCount = 0;
+                pixel = random(NUMPIXEL);
+            }
+            break;
+        case 2:
+            period = 1500;
+            brightness = pattern1(flashCount*cycle/period);
+            flashCount++;
+            if(pause == 0)
+            {
+                color = strip.Color(brightness*maxbrightness,brightness*maxbrightness,brightness*maxbrightness);
+                if(flashCount * cycle >= period)
+                {
+                    flashCount = 0;
+                    pause = 1000 * random(2, 15);
+                }
+            }
+            else
+            {
+                color = strip.Color(0,0,0);
+                if(flashCount * cycle >= pause)
+                {
+                    flashCount = 0;
+                    pause = 0;
+                }
+            }
+            setAllPixels(color);
+            break;
+        case 3:
+            color = strip.Color(0,0,0);
+            setAllPixels(color);
+            break;
+        case 4:
+            color = strip.Color(0,0,0);
+            setAllPixels(color);
+            break;
+        case 5:
+            color = strip.Color(0,0,0);
+            setAllPixels(color);
+            break;
+        case 6:
+            color = strip.Color(0,0,0);
+            setAllPixels(color);
+            break;
+        case 7:
+            color = strip.Color(0,0,0);
+            setAllPixels(color);
+            break;
+        case 8:
+            color = strip.Color(0,0,0);
+            setAllPixels(color);
+            break;
+        case 9:
+            color = strip.Color(0,0,0);
+            setAllPixels(color);
+            break;
+        default:
+            color = strip.Color(0,0,0);
+            setAllPixels(color);
+            break;
+    }
+    strip.show();
 }
 
 /*****Loop*****/
 void loop()
 {
-    switch(dekade())
+    dekade = dekadenschalter();
+    switch(dekade)
     {
         case 1:
             break;
@@ -91,13 +182,13 @@ void loop()
 }
 
 /*****Dekade*****/
-unsigned int dekade()
+unsigned int dekadenschalter()
 {
     bool bit1 = !digitalRead(DEK2);
     bool bit2 = !digitalRead(DEK4);
     bool bit3 = !digitalRead(DEK8);
     bool bit4 = !digitalRead(DEK16);
-    unsigned int dekade = (((((bit4 << 1) + bit3) << 1) + bit2) << 1) + bit1;
+    dekade = (((((bit4 << 1) + bit3) << 1) + bit2) << 1) + bit1;
     if(dekade != letzte_dekade)
     {
         letzte_dekade = dekade;
@@ -114,7 +205,7 @@ unsigned int dekade()
 }
 
 /*****setAllPixels*****/
-void setAllPixels(uint16_t color)
+void setAllPixels(uint32_t color)
 {
     for(uint16_t i=0;i<NUMPIXEL;i++)
     {
@@ -123,7 +214,7 @@ void setAllPixels(uint16_t color)
 }
 
 /*****setPixelRow*****/
-void setPixelRow(int row, uint16_t color)
+void setPixelRow(int row, uint32_t color)
 {
     for(uint16_t i=0;i<COLUMNS;i++)
     {
@@ -132,10 +223,24 @@ void setPixelRow(int row, uint16_t color)
 }
 
 /*****setPixelColumn*****/
-void setPixelColumn(int column, uint16_t color)
+void setPixelColumn(int column, uint32_t color)
 {
     for(uint16_t i=0;i<ROWS;i++)
     {
         strip.setPixelColor(ledmatrix[i][column], color);
     }
+}
+
+/*****pattern1*****/
+/****flash the whole cloud****/
+float pattern1(float num)
+{
+  return ((cos((num)*3.2*M_PI)+cos((1.5*num)*3.2*M_PI))+2)/4;;
+}
+
+/*****pattern2*****/
+/****dim down and back up****/
+float pattern2(float num)
+{
+  return (-sin((num)*1.0*M_PI)+1);
 }
